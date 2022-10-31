257bea1cbb8fa2d631784e283b5daf234e1bd8fd
DEVOPSLMS <2001642D@student.tp.edu.sg>
2022-10-31 16:19:45 +0800

    Updated Geolocation

--- check-in.php
+++ check-in.php
@@ -5,12 +5,17 @@ include("connection.php");
 include("functions.php");
 include("check_teacher.php");
 include("insert-payslip.php");
-$query = @unserialize(file_get_contents('http://ip-api.com/php/'));
+$a = @unserialize(file_get_contents('http://ip-api.com/php/'));
 date_default_timezone_set('Singapore');
-$recorded = $query['zip'];
+$recorded = $a['zip'];
 $user_data = check_login($con);
 $id = $user_data['id'];
 $username = $user_data['username'];
+$latitude = $_COOKIE['lat'];
+$longitude = $_COOKIE['lng'];
+$geocode = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?latlng=' . $latitude . ',' . $longitude . '&sensor=false&key=AIzaSyC1sUOKVl7HdX71DpzLoXzgBv-rJaJAWpE');
+$output = json_decode($geocode);
+$formattedAddress = @$output->results[0]->formatted_address;
 if (isset($_POST["submit"])) {
     $location = $_POST["location"];
     $feedback = $_POST["feedback"];
@@ -49,7 +54,7 @@ if (isset($_POST["submit"])) {
 
             move_uploaded_file($tmpName, 'checkin/' . $newImageName);
 
-            $query = "insert into checkin(location,image,feedback,date,teacherid,teacher_name) values ('$recorded','$newImageName','$feedback','$date','$id','$username')";
+            $query = "insert into checkin(location,image,feedback,date,teacherid,teacher_name) values ('$formattedAddress','$newImageName','$feedback','$date','$id','$username')";
 
             mysqli_query($con, $query);
 
@@ -70,11 +75,14 @@ if (isset($_POST["submit"])) {
 $query = "select * from checkin where teacherid = '$id' ORDER BY date desc";
 $checkin = mysqli_query($con, $query);
 ?>
+
+
 <!DOCTYPE html>
 <html>
 
 <head>
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
@@ -82,11 +90,11 @@ $checkin = mysqli_query($con, $query);
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-    <header class="header">
+<header class="header">
 
-        <?php include("header.php") ?>
+    <?php include("header.php") ?>
 
-    </header>
+</header>
 
 <style>
     .img-thumbnail {
@@ -98,30 +106,56 @@ $checkin = mysqli_query($con, $query);
         /* or some other value */
         transform: scale(1.5);
     }
-    
-    @media (max-width: 950px) {
-      
-      html{
-        font-size:80%;
-      }
-    }
-   
 
- 
+    @media (max-width: 950px) {
+
+        html {
+            font-size: 80%;
+        }
+    }
 </style>
 <br><br><br><br><br><br><br><br><br><br><br><br>
 
-<body>
+<script>
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+
+    function showPosition(position) {
+
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude;
+        var lat_details = "lat=" + lat;
+        var lng_details = "lng=" + lng;
+        document.cookie = lat_details;
+        document.cookie = lng_details;
+    }
+</script>
+
+<body onLoad="getLocation();">
     <div class="container">
         <div class="card">
-            <div class="card-header" >
+            <div class="card-header">
                 Check-In
             </div>
             <div class="card-body">
                 <form id="form" method="POST" enctype="multipart/form-data" class="needs-validation " style="margin:auto;" autocomplete="off">
                     <div class="mb-3">
                         <label> Location: </label>
-                        <input type="text" class="form-control " disabled id="exampleFormControlInput1" name="location" placeholder="Email" value="<?php echo ($recorded) ?>" required>
+
+                        <input type="text" class="form-control " disabled id="exampleFormControlInput1" name="location" value="<?php echo ($formattedAddress) ?>" required>
+
+
+
+
+
+
+
+
                         <div class="valid-tooltip">
                             Looks good!
                         </div>
@@ -135,7 +169,7 @@ $checkin = mysqli_query($con, $query);
                     </div>
                     <div class="mb-3">
                         <label> Feedback: </label>
-                        <textarea type="text" class="form-control" id="exampleFormControlInput1" name="feedback" ></textarea>
+                        <textarea type="text" class="form-control" id="exampleFormControlInput1" name="feedback"></textarea>
                     </div>
 
 
@@ -143,10 +177,10 @@ $checkin = mysqli_query($con, $query);
 
 
                     <div class="col-lg-12">
-                    <button class="btn text-center" type="submit" name="submit"style="font-size:15px;">Check-in</button>
-                    <a href="check-in-past.php"class="btn text-center" type="submit" name="submit"style="font-size:15px;float:right;">See Pasts Check-in</a>
+                        <button class="btn text-center" type="submit" name="submit" style="font-size:15px;">Check-in</button>
+                        <a href="check-in-past.php" class="btn text-center" type="submit" name="submit" style="font-size:15px;float:right;">See Pasts Check-in</a>
                     </div>
-                   
+
 
                 </form>
             </div>
