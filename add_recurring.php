173024896835570f1c82b3c77c84eb3c77a54bc9
izzul-hilmi <2001177A@student.tp.edu.sg>
2022-11-21 22:58:02 +0800

    Added sec 1-5

--- add_recurring.php
+++ add_recurring.php
@@ -151,6 +151,11 @@ if (isset($_POST["add"])) {
                             <option value="P5(F)">P5(F)</option>
                             <option value="P6(N)">P6(N)</option>
                             <option value="P6(F)">P6(F)</option>
+                            <option value="sec_1">Sec 1</option>
+                            <option value="sec_2">Sec 2</option>
+                            <option value="sec_3">Sec 3</option>
+                            <option value="sec_4">Sec 4</option>
+                            <option value="sec_5">Sec 5</option>
 
                         </select>
                     </div>
